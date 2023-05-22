6f3a251c3d8aa61e1115119cc83aeb826e2b806a
keedem <liugoldent@gmail.com>
2023-05-22 13:44:30 +0800

    add

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World. Welcome to FastAPI!"}
+
+
+@app.get("/path")
+async def demo_get():
+    return {"message": "This is /path endpoint, use a post request to transform the text to uppercase"}
+
+
+@app.post("/path")
+async def demo_post(inp: Msg):
+    return {"message": inp.msg.upper()}
+
+
+@app.get("/path/{path_id}")
+async def demo_get_path_id(path_id: int):
+    return {"message": f"This is /path/{path_id} endpoint, use post request to retrieve result"}
