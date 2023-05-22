a93049c481e82bd92c0cf9b412bcb2e4dfb07296
keedem <liugoldent@gmail.com>
2023-05-22 14:11:55 +0800

    add

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+
+
+class Msg(BaseModel):
+    msg: str
 
 
 @app.get("/")
